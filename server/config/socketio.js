55a3d806425bc50138c7693e4f39496992daa970
alimfeld <aimfeld@gmx.ch>
2014-10-28 23:09:37 +0100

    Removed references to deleted 'things'

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -20,7 +20,6 @@ function onConnect(socket) {
   // Insert sockets below
   require('../api/competition/competition.socket').register(socket);
   require('../api/league/league.socket').register(socket);
-  require('../api/thing/thing.socket').register(socket);
 }
 
 module.exports = function (socketio) {
